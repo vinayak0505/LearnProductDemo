ebadc9f707a47cf57e284b60191334185c2c7bb1
Vinayak Aggarwal <vinayakaggarwal05@gmail.com>
2024-07-07 11:13:40 +0530

    added bycript encription for password

--- src/main/java/com/vinayak/product_demo/ApplicationSecurityConfiguration.java
+++ src/main/java/com/vinayak/product_demo/ApplicationSecurityConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -20,7 +20,7 @@ public class ApplicationSecurityConfiguration {
     protected AuthenticationProvider authProvider(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
-        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        provider.setPasswordEncoder(new BCryptPasswordEncoder());
         return provider;
     }
 
